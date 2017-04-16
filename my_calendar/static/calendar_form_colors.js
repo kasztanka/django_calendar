6f9b950a976d0ba022e7a095bec6b9352faf9030
piotermiarer <piotr@atopik.pl>
2017-04-16 19:59:36 +0200

    Create calendar template and minor indentation changes

--- my_calendar/static/calendar_form_colors.js
+++ my_calendar/static/calendar_form_colors.js
@@ -1,10 +1,10 @@
-const colors = document.getElementsByClassName("colors");
+const colors = document.getElementsByClassName("color-box");
 
 Array.prototype.slice.call(colors).forEach(function(color) {
     color.onclick = function() {
-        let old = document.getElementsByClassName("active")[0];
-        old.className = "colors";
-        this.className += " active";
+        let old = document.getElementsByClassName("active-color-box")[0];
+        old.classList.remove("active-color-box");
+        this.classList.add("active-color-box");
         input.value = this.getAttribute("name");
     };
 });
